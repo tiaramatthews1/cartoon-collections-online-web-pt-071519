63b73ea250fc51d50c96b200f941a57eea5d3557
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-23 18:59:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,15 +2,15 @@ def roll_call_dwarves(dwarves)
 
 end
 
-def summon_captain_planet
+def summon_captain_planet()
   
 end
 
-def long_planeteer_calls
+def long_planeteer_calls()
   
 end
 
-def find_the_cheese(no_cheese)
+def find_the_cheese()
   
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
