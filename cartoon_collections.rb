023f8f5883f31f8f6167bdc50838ee1800069e14
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-23 19:35:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  if array.any? {|word| word.length > 4}
+    return true
+    else return false 
+end
 end
 
 def find_the_cheese(array)
